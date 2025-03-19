04a0413b4414e590fbb28159dda460bf68f3e134
mainocean <mainoceanm@gmail.com>
2025-03-19 18:13:34 +0200

    upd2 add files

---
+++ lessons/002/terraform/0-locals.tf
@@ -0,0 +1,10 @@
+locals {
+  project_id = "Your_progect_ID" # rename to Your progect ID
+  region     = "us-central1"
+  apis = [
+    "compute.googleapis.com",
+    "container.googleapis.com",
+    "logging.googleapis.com",
+    "secretmanager.googleapis.com"
+  ]
+}
